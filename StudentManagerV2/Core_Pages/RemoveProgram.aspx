8ec80507bda0211daaa2d714b537d90d9d6f9b2b
wpritchard1 <wpritchard1@WB206-22>
2016-11-29 15:41:09 -0700

    Updated Master page, config and added northwind db for debugging

--- StudentManagerV2/Core_Pages/RemoveProgram.aspx
+++ StudentManagerV2/Core_Pages/RemoveProgram.aspx
@@ -1,6 +1,4 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPages/Core.Master" Theme="Query" CodeBehind="RemoveProgram.aspx.cs" Inherits="StudentManager.RemoveProgram" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="banner" runat="server">
-</asp:Content>
 <asp:Content ID="Remove_Program" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h2>Remove a Program</h2>
         <asp:TextBox runat="server" ID="Program_Code">
--- StudentManagerV2/MasterPages/Core.Master.designer.cs
+++ StudentManagerV2/MasterPages/Core.Master.designer.cs
@@ -38,5 +38,14 @@ namespace StudentManager.MasterPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Footer;
+        
+        /// <summary>
+        /// ErrorMSG control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ErrorMSG;
     }
 }
