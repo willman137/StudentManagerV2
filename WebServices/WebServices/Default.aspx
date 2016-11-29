f1cb278e5b5d8dccd8ad7140a89d1dcd1e18661e
wpritchard1 <wpritchard1@WB206-22>
2016-11-29 15:57:13 -0700

    Added in temp project, add in web service code then delete the whole thing. Remember to add teh reference from the webbaist.nait.ca /students/wpritchard1 area

---
+++ WebServices/WebServices/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebServices.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="grid" runat="server">
+
+        </asp:GridView>
+        <br/>
+        <asp:Label ID="binary" runat="server"></asp:Label>
+        <br/>
+        <asp:Label ID="listen" runat="server"></asp:Label>
+        <br/>
+        <asp:Label ID="closely" runat="server"></asp:Label>
+        <br/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebServices/WebServices/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebServices
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            localhost.CoreService proxy = new localhost.CoreService();
+
+            DataSet table = proxy.GetCustomersByCountry("USA");
+            grid.DataSource = table;
+            grid.DataBind();
+            int g = 42;
+            binary.Text = g+" To Binary: "+proxy.DecimalToBinary(g);
+
+            int t = 01110101;
+            listen.Text = t + " To Decimal: " + proxy.BinaryToDecimal(t);
+
+            int h = 43;
+            closely.Text = "Is " + h + " a prime #? " + (proxy.IsItPrime(h) ? "Yes": "No");
+                }
+    }
+}
