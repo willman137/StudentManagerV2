a07af10ad89077a842f7378cac71eb6af2d22155
wpritchard1 <wpritchard1@WB206-22>
2016-11-29 16:17:10 -0700

    Updated all the goodies

---
+++ StudentManagerV2/MyWebService.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace StudentManagerV2
+{
+    /// <summary>
+    /// Summary description for MyWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod(Description = "Gets A list of country codes")]
+        public void GetListOfCustomersByCountryCode(string countrycode) {
+            SqlConnection theConnection = new SqlConnection();
+            theConnection.ConnectionString = ConfigurationManager.ConnectionStrings["NorthwindContextManager"].ConnectionString;
+        }
+    }
+}
